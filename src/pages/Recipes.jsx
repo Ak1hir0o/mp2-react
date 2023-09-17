5b176093dc7cbed5f8412c90b367f28f4a6c508a
Jonnel Ladera <126427890+Ak1hir0o@users.noreply.github.com>
2023-09-17 11:23:41 +0800

    updating the recipe api

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -9,6 +9,7 @@ import {
   Col,
   Card,
   Button,
+  Form,
 } from "react-bootstrap";
 import "../styles/Recipe.css";
 
@@ -20,62 +21,150 @@ const Recipes = () => {
     useState(null);
   const [categoryResults, setCategoryResults] =
     useState([]);
+  const [searchResults, setSearchResults] =
+    useState([]);
 
   useEffect(() => {
-    fetch(
-      "https://www.themealdb.com/api/json/v1/1/categories.php"
-    )
-      .then((response) => response.json())
-      .then((data) =>
-        setCategories(data.categories)
-      )
-      .catch((error) => console.log(error));
+    fetchCategories();
   }, []);
+
+  const fetchCategories = async () => {
+    try {
+      const response = await axios.get(
+        "https://www.themealdb.com/api/json/v1/1/categories.php"
+      );
+      setCategories(response.data.categories);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const handleCategoryClick = (category) => {
     setSelectedCategory(category);
-    // Make another API call to get the results for the selected category
-    fetch(
-      `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category.strCategory}`
-    )
-      .then((response) => response.json())
-      .then((data) =>
-        setCategoryResults(data.meals)
-      )
-      .catch((error) => console.log(error));
+    fetchCategoryResults(category.strCategory);
   };
 
-  const openDetailsWindow = (result) => {
-    fetch(
-      `https://www.themealdb.com/api/json/v1/1/search.php?s=${result.strMeal}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const details = data.meals[0];
-        const detailsWindow = window.open(
-          "",
-          "_blank"
-        );
-        detailsWindow.document.write(`
-          <html>
-            <head>
-              <title>Meal Details</title>
-            </head>
-            <body>
-              <h1>${details.strMeal}</h1>
-              <img src="${details.strMealThumb}" alt="${details.strMeal}" />
-              <p><strong>Ingredients:</strong> ${details.strIngredient1}, ${details.strIngredient2}, ${details.strIngredient3}, ...</p>
-              <p><strong>Procedure:</strong> ${details.strInstructions}</p>
-            </body>
-          </html>
-        `);
-        detailsWindow.document.close();
-      })
-      .catch((error) => console.log(error));
+  const fetchCategoryResults = async (
+    category
+  ) => {
+    try {
+      const response = await axios.get(
+        `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`
+      );
+      setCategoryResults(response.data.meals);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSearch = async (searchTerm) => {
+    // Check if the search term contains numbers or special characters
+    const regex = /^[a-zA-Z\s]+$/;
+    if (!regex.test(searchTerm)) {
+      alert(
+        "Invalid search term. Please enter only letters and spaces."
+      );
+      return;
+    }
+
+    try {
+      const response = await axios.get(
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`
+      );
+      if (response.data.meals === null) {
+        alert("No results found.");
+      } else {
+        setSearchResults(response.data.meals);
+        setSelectedCategory(null);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const openDetailsWindow = async (result) => {
+    try {
+      const response = await axios.get(
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${result.strMeal}`
+      );
+      const details = response.data.meals[0];
+      const detailsWindow = window.open(
+        "",
+        "_blank"
+      );
+      detailsWindow.document.write(`
+        <html>
+          <head>
+            <title>Meal Details</title>
+          </head>
+          <body>
+            <h1>${details.strMeal}</h1>
+            <img src="${details.strMealThumb}" alt="${details.strMeal}" />
+            <p><strong>Ingredients:</strong> ${details.strIngredient1}, ${details.strIngredient2}, ${details.strIngredient3}, ...</p>
+            <p><strong>Procedure:</strong> ${details.strInstructions}</p>
+          </body>
+        </html>
+      `);
+      detailsWindow.document.close();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <Container>
+      <h1>Recipe Search</h1>
+      <Form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const searchTerm =
+            event.target.elements.searchTerm
+              .value;
+          handleSearch(searchTerm);
+        }}>
+        <Form.Group controlId="searchForm">
+          <Form.Control
+            type="text"
+            name="searchTerm"
+            placeholder="Search for a recipe"
+          />
+          <Button type="submit">Search</Button>
+        </Form.Group>
+      </Form>
+
+      {searchResults.length > 0 && (
+        <>
+          <h2>Search Results</h2>
+          <Row>
+            {searchResults.map((result) => (
+              <Col
+                key={result.idMeal}
+                sm={6}
+                md={4}
+                lg={3}>
+                <Card>
+                  <Card.Img
+                    variant="top"
+                    src={result.strMealThumb}
+                  />
+                  <Card.Body>
+                    <Card.Title>
+                      {result.strMeal}
+                    </Card.Title>
+                    <Button
+                      onClick={() =>
+                        openDetailsWindow(result)
+                      }>
+                      View Details
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
+
       <h1>Categories</h1>
       <Row>
         {categories.map((category) => (
@@ -143,4 +232,5 @@ const Recipes = () => {
     </Container>
   );
 };
+
 export default Recipes;
