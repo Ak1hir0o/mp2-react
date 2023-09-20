395cfa0baf311bb68cf9e34a68b6616389dba0f9
Jonnel Ladera <126427890+Ak1hir0o@users.noreply.github.com>
2023-09-20 21:34:37 +0800

    adding css on navbarr and footer

--- src/UI/Footer.jsx
+++ src/UI/Footer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BsTwitter } from "react-icons/bs";
 import { BsInstagram } from "react-icons/bs";
 import { BsFacebook } from "react-icons/bs";
+import { Link } from "react-router-dom";
 import "../styles/Footer.css";
 
 const Footer = () => {
@@ -10,6 +11,12 @@ const Footer = () => {
       <div className="socialMedia">
         <BsInstagram /> <BsTwitter />
         <BsFacebook />
+      </div>
+      <div className="quickLinks">
+        <Link to="/">Home</Link>
+        <Link to="/recipe">Recipe</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
       </div>
       <p> &copy; Copyright 2022</p>
     </div>
