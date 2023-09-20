1610d34fb49fd6580a02530b94b850910a61e74b
AabcdefU <johnaaron.delacruz@yahoo.com>
2023-09-20 18:50:24 +0800

    About and Contacts edit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,7 @@
 import React from "react";
-import {
-  Container,
-  Row,
-  Col,
-} from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import SampleImg from "../assets/sample_img.jpg";
-import SampleImg2 from "../assets/sample_img_2.jpg";
+import SampleImg2 from "../assets/sample_img2.jpg";
 import "../styles/About.css";
 
 const About = () => {
@@ -21,15 +17,11 @@ const About = () => {
               <h1>About Us</h1>
               <h5>Our Mission</h5>
               <p>
-                At Foodie, we are dedicated to
-                celebrating the world's diverse
-                culinary traditions and connecting
-                food enthusiasts with the flavors
-                and stories that make every bite
-                an unforgettable experience. Our
-                mission is to inspire and empower
-                people to explore, appreciate, and
-                savor the rich tapestry of global
+                At Foodie, we are dedicated to celebrating the world's diverse
+                culinary traditions and connecting food enthusiasts with the
+                flavors and stories that make every bite an unforgettable
+                experience. Our mission is to inspire and empower people to
+                explore, appreciate, and savor the rich tapestry of global
                 cuisine.
               </p>
             </div>
@@ -40,26 +32,17 @@ const About = () => {
             <div className="about-text">
               <h5>Our Story</h5>
               <p>
-                Founded in 2023, Foodie emerged
-                from a shared love for food and a
-                desire to share that passion with
-                the world. What started as a
-                humble blog has evolved into a
-                thriving community of food lovers,
-                chefs, and artisans who believe
-                that food is more than sustenance;
-                it's an art form, a cultural
-                touchstone, and a source of
-                endless fascination.
+                Founded in 2023, Foodie emerged from a shared love for food and
+                a desire to share that passion with the world. What started as a
+                humble blog has evolved into a thriving community of food
+                lovers, chefs, and artisans who believe that food is more than
+                sustenance; it's an art form, a cultural touchstone, and a
+                source of endless fascination.
               </p>
             </div>
           </Col>
           <Col md={6}>
-            <img
-              src={SampleImg2}
-              className="img2"
-              alt=""
-            />
+            <img src={SampleImg2} className="img2" alt="" />
           </Col>
         </Row>
       </section>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,62 +1,56 @@
 import React from "react";
-import {
-  Container,
-  Row,
-  Col,
-  Form,
-  Button,
-} from "react-bootstrap";
+import { useState } from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import "../styles/Contact.css";
 
 const Contact = () => {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
   return (
     <Container className="contact-section">
       <h1>Contact Us</h1>
-      <Form className="contact-form">
+      <Form
+        noValidate
+        validated={validated}
+        onSubmit={handleSubmit}
+        className="contact-form"
+      >
         <Row>
           <Col>
             <Form.Control
+              required
               type="text"
               className="contact-form-text"
               placeholder="Your Name"
             />
-          </Col>
-          <Col>
             <Form.Control
+              required
               type="email"
               className="contact-form-text"
               placeholder="Your E-mail"
             />
-          </Col>
-          <Col>
             <Form.Control
-              type="text"
+              required
+              type="number"
               className="contact-form-text"
               placeholder="Your Phone Number"
             />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
             <Form.Control
+              required
               as="textarea"
               className="contact-form-text"
               placeholder="Your Message"
             />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Button
-              type="submit"
-              className="contact-form-btn">
-              Submit
-            </Button>
-          </Col>
-          <Col>
-            <Button
-              type="submit"
-              className="contact-form-btn">
+            <Button type="submit" className="contact-form-btn">
               Submit
             </Button>
           </Col>
